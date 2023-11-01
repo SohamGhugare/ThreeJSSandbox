216daeb41ad5e67b8b4fa2aa994d0f560c4e7b02
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-01 23:52:01 +0530

    feat: added dat.gui menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 // renderer
@@ -32,9 +33,27 @@ const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(box);
 
+const gridHelper = new THREE.GridHelper();
+scene.add(gridHelper);
+
+// dat.gui
+const gui = new dat.GUI();
+
+const options = {
+    boxColor: '#00ff00',
+    speed: 1
+};
+
+gui.addColor(options, 'boxColor').onChange(function(e){
+    box.material.color.set(e);
+});
+
+gui.add(options, 'speed', 1, 10);
+
+
 function animate(time) {
     // setting the rotation
-    box.rotation.set(time/1000, time/1000, 0);
+    box.rotation.set(time/1000*options.speed, time/1000*options.speed, 0);
 
     // linking scene and camera
     renderer.render(scene, camera);   
