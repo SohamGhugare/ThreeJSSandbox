e412f433e57b2e49a7bb1a10223403d5d0b9af24
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-01 23:26:49 +0530

    feat: added scene and perp camera

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,18 @@
 import * as THREE from 'three';
 
+// renderer
 const renderer = new THREE.WebGLRenderer();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
+
+// scene
+const scene = new THREE.scene();
+
+// camera: perspective
+const camera = new THREE.PerspectiveCamera(
+    75,
+    windwo.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+
